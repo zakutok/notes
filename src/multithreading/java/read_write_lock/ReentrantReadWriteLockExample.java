18347ab5e2a5a809f80c3f719f8f24d1cba3fe05
zakutok <>
2022-09-05 21:18:47 +0300

    test

--- src/multithreading/java/read_write_lock/ReentrantReadWriteLockExample.java
+++ src/multithreading/java/read_write_lock/ReentrantReadWriteLockExample.java
@@ -1,15 +1,54 @@
 package read_write_lock;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReentrantReadWriteLockExample {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        var res = new ArrayList<>(List.of(1, 2));
+
         var reentrantReadWriteLock = new ReentrantReadWriteLock();
 
         var readLock = reentrantReadWriteLock.readLock();
         var writeLock = reentrantReadWriteLock.writeLock();
 
-        readLock.lock();
-        readLock.unlock();
+        Callable<Void> readableTask = () -> {
+            Thread.sleep(ThreadLocalRandom.current().nextLong(1_000, 4_000));
+            readLock.lock();
+            System.out.println("in read lock: readLockCount = " + reentrantReadWriteLock.getReadLockCount() + " isWriteLocked = " + reentrantReadWriteLock.isWriteLocked() + " " + res);
+            Thread.sleep(ThreadLocalRandom.current().nextLong(1_000));
+            readLock.unlock();
+            return null;
+        };
+
+        Callable<Void> writableTask = () -> {
+            Thread.sleep(ThreadLocalRandom.current().nextLong(1_000, 3_000));
+            writeLock.lock();
+            res.add(res.get(res.size() - 1) + 1);
+            System.out.println("in write lock: readLockCount = " + reentrantReadWriteLock.getReadLockCount() + " isWriteLocked = " + reentrantReadWriteLock.isWriteLocked() + " " + res);
+            Thread.sleep(ThreadLocalRandom.current().nextLong(3_000));
+            writeLock.unlock();
+            return null;
+        };
+
+        var readThreadsNumber = 15;
+        var writeThreadsNumber = 5;
+        var nThreads = readThreadsNumber + writeThreadsNumber;
+        var executorService = Executors.newFixedThreadPool(nThreads);
+
+        var tasks = new ArrayList<>(Collections.nCopies(readThreadsNumber, readableTask));
+        tasks.addAll(Collections.nCopies(writeThreadsNumber, writableTask));
+
+        executorService.invokeAll(tasks);
+        executorService.shutdown();
     }
 }
